db69c4cc749c0510591f7d822d4a5c3ee3fb24ec
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2024-07-02 20:00:58 -0300

    initial commit
    - initial project configuration with Java version 22 and Spring Boot version 3.3.1
    - initialized version control with Git

---
+++ src/main/java/com/rosivaldolucas/magalu_connect/MagaluConnectApplication.java
@@ -0,0 +1,13 @@
+package com.rosivaldolucas.magalu_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagaluConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagaluConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rosivaldolucas/magalu_connect/MagaluConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosivaldolucas.magalu_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagaluConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
